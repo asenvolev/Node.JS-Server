79d78936fb808d06e968839c1c11660e8a22ff79
asen.v <asen.v@sbtech.com>
2020-07-12 14:14:17 +0000

    implement first simple get from DB in a request and check if collection does not exist then create it and if no records are in there insert one and then get it and send it to FE

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,18 +1,33 @@
 const utils = require('../utilities');
+const mongo = require('../mongoUtills');
+const SimpleModel = require('../models/SimpleModel');
 
 const actions = {
-  'GET': (request, response) => {
-    utils.sendResponse(response, 'Hello World', 200, {'Content-Type': 'text/plain'});
+  'GET': async (request, response) => {
+    var db = mongo.getDb();
+    let messages = db.collection('messages');
+    if (!messages) {
+      await db.createCollection('messages');
+    }
+    let count = await messages.find().count();
+    if (count == 0) {
+      let student = new SimpleModel("Hello World");
+      await db.collection('messages').insertOne(student);
+    }
+    const messageToRender = await messages.find().toArray();
+    console.log(messageToRender);
+    utils.sendResponse(response, messageToRender.toString(), 200, {'Content-Type': 'text/plain'});
   },
   'POST': (request, response) => {
     utils.collectData(request, (formattedData) => {
+
       // do something with the formatted data e.g. store in db
       utils.sendResponse(response, 'Success', 200, {'Content-Type': 'text/plain'});
     });
   }
 };
 
-module.exports = (request, response) => {
+module.exports = (request, response) => {   //TO DO this restricts a contoroller to have up to one function of every request method. Should be extended or refactored.
     var action = actions[request.method];
     if (action) {
       action(request, response);
---
+++ models/SimpleModel.js
@@ -0,0 +1,5 @@
+class SimpleModel {
+    constructor(message){
+        this.message = message
+    }
+};
